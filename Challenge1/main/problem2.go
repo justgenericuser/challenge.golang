c9a0f1db16065d8ab5bed2fcf85159817f639a7d
Code monkey <45602089+justgenericuser@users.noreply.github.com>
2018-12-10 18:29:41 +0100

    Solution Submission
    GoChallenge: Committing the working solutions for both problem1 and problem2

--- Challenge1/main/problem2.go
+++ Challenge1/main/problem2.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"log"
+	"sync"
+	"fmt"
 	"time"
 	"math/rand"
 )
+func main() {
+	problem2()
+}
+
+
 
 func problem2() {
 
@@ -18,9 +25,17 @@ func problem2() {
 	// is printed.
 	//
 
-	for inx := 0; inx < 10; inx++ {
+	// create a new instance of a sync.WaitGroup, named wg
+	var wg sync.WaitGroup
 
-		go printRandom2(inx)
+
+	//creating the ticker with 1sec as time interval
+	ticker := time.NewTicker(1000 * time.Millisecond)
+
+	for inx := 0; inx < 10; inx++ {
+		// add another go routine to WaitGroup in order to wait for it to finish executing before method problem1 finishes its execution
+		wg.Add(1)
+		go printRandom2(inx, &wg, *ticker)
 
 	}
 
@@ -34,16 +49,24 @@ func problem2() {
 	// Same as problem1...
 	//
 
-	time.Sleep(5 * time.Second)
+	//time.Sleep(5 * time.Second)
+	// wait for all the go routines to finish
+	wg.Wait()
 
 	log.Printf("problem2: finished -------------------------------------------")
 }
 
-func printRandom2(slot int) {
+func printRandom2(slot int, wg *sync.WaitGroup, ticker time.Ticker) {
+	// schedule the call to Done to tell func problem2 we are done
+	// defer is used to call the wg.Done() method at the end of the enclosing function, i.e., printRandom2
+	defer wg.Done()
 
 	for inx := 0; inx < 10; inx++ {
 
-		log.Printf("problem2: slot=%03d count=%05d rand=%f", slot, inx, rand.Float32())
-
+		for t := range ticker.C {
+			fmt.Println("problem2: Tick at", t, "slot =", slot, "count =", inx, "rand =",rand.Int())
+			//log.Printf("problem2: slot=%03d count=%05d rand=%f", slot, inx, rand.Float32())
+			break
+		}
 	}
 }
